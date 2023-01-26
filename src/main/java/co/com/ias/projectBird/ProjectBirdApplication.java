a76aac77f3a2810cf2aeb90f62cd494a23307081
SantiagoVelasquezMontoya <santiago.velasquez@ias.com.co>
2023-01-26 15:11:17 -0500

    Clean Architecture Setup

---
+++ src/main/java/co/com/ias/projectBird/ProjectBirdApplication.java
@@ -0,0 +1,13 @@
+package co.com.ias.projectBird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBirdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ias/projectBird/ProjectBirdApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ias.projectBird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
